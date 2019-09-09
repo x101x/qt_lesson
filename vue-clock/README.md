83569eb4bf35dd094fa63e1cfb26557382544a51
x101x <1642372486@qq.com>
2019-09-09 18:57:55 +0800

    VUE-clock

---
+++ vue-clock/README.md
@@ -0,0 +1,24 @@
+# vue-clock
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+- 组件化思想
+    前端重要的思想革命 components/
+    页面开发更简单  交给不同组件去做
---
+++ vue-clock/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Second :second="second" />
+    <Minute />
+    <Hour />
+    <Apm />
+    <Week />
+    <Day />
+    <Mouth />
+  </div>
+</template>
+
+<script>
+// @=src alisa 短链接
+import Second from "@/components/Second";
+import Minute from "@/components/Minute";
+import Hour from "@/components/Hour";
+import Apm from "@/components/Apm";
+import Week from "@/components/Week";
+import Day from "@/components/Day";
+import Mouth from "@/components/Mouth";
+// import Second from '@/components/Second'
+
+export default {
+  name: "App",
+  components: {
+    Second,
+    Minute,
+    Hour,
+    Apm,
+    Week,
+    Day,
+    Mouth
+  },
+  data() {
+    return {
+      second:''
+    }
+  },
+  methods:{
+start(){
+    setInterval(()=>{
+      let date = new Date();
+      this.second =date.getSeconds();
+    },1000)
+  }
+  },
+  
+  created(){
+  this.start();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
