fde432a8d0a449533996b24b2c0d9c60942591b8
x101x <1642372486@qq.com>
2019-10-22 14:29:14 +0800

    caroysel

---
+++ vue/caroysel/README.md
@@ -0,0 +1,29 @@
+# caroysel
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## 组件设计
+面向组件编程 设计优秀的组件
+Carousel   幻灯片npm
+- 滴滴 cube-ui    
+特殊的业务场景 打车  
+- 幻灯片组件 全站做的统一组件     
+    完成哪些功能？                                                                                                                  
---
+++ vue/caroysel/src/components/EcSlide.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="switch-img-box" id="ec-slide-box" :style="{width:swidth +'px'}">幻灯片</div>
+</template>
+
+<script>
+export default {
+  name: "EcSlide",
+  //组件内部数据
+  data() {
+    return {
+      timer: null //定时器 组件自己打整
+    };
+  },
+  //外界的传参
+  // props:['time','autoplay']
+  props: {
+    time: {
+      type: Number,
+      default: 4000
+    },
+    autoplay: {
+      type: Boolean
+    },
+    swidth:{
+      type:Number,
+      // default:800
+    }
+  },
+  //计算属性
+  computed: {
+    
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  },
+};
+</script>
+
+<style lang='stylus' scoped>
+// stylus stylus-loader
+.switch-img-box
+  width 100%
+  height 100%
+  position relative
+  
+</style>
