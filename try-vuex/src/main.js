869282eb78b7afe4a58b2be0eb371c0ba63ade9f
x101x <1642372486@qq.com>
2019-10-31 14:31:36 +0800

    vuex

---
+++ try-vuex/src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import MyVuex from './myvuex'
+import { stat } from 'fs'
+
+Vue.config.productionTip = false
+
+// 单一状态树
+let store = new MyVuex.Store({
+  state: {
+    count: 0
+  },
+  getters: { 
+    getStatePlusOne(state){
+        return state.count+1
+    }
+  }
+}, Vue)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>'
+})
